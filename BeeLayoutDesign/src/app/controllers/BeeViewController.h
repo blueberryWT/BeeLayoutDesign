274a746fe279aec3281527db0771b8ce6d6fa7b1
sky <b.sky@163.com>
2013-05-02 14:12:04 +0800

    测试创建文件

---
+++ BeeLayoutDesign/src/app/controllers/BeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BeeViewController.h
+//  BeeLayoutDesign
+//
+//  Created by apple on 13-5-2.
+//  Copyright (c) 2013年 Bee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BeeViewController : NSViewController
+
+@end
---
+++ BeeLayoutDesign/src/app/controllers/BeeViewController.m
@@ -0,0 +1,27 @@
+//
+//  BeeViewController.m
+//  BeeLayoutDesign
+//
+//  Created by apple on 13-5-2.
+//  Copyright (c) 2013年 Bee. All rights reserved.
+//
+
+#import "BeeViewController.h"
+
+@interface BeeViewController ()
+
+@end
+
+@implementation BeeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
